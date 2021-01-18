072cf45434552925349312316b5c3f3657390804
hcnt <jacek.kusmierczyk1@gmail.com>
2021-01-19 00:45:17 +0100

    debug false

--- mimcal/settings.py
+++ mimcal/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '1afc8#u(_gfw3@no_w&jawr8ibqhasxh=(0#trt9y4w5$yu)4_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
