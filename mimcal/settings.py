ee13b68a241a1d74dde2c0e0889ad846b93f875b
hcnt <jacek.kusmierczyk1@gmail.com>
2021-01-19 01:07:54 +0100

    remove secret

--- mimcal/settings.py
+++ mimcal/settings.py
@@ -18,9 +18,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1afc8#u(_gfw3@no_w&jawr8ibqhasxh=(0#trt9y4w5$yu)4_'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
