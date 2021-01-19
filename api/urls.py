268967f6d055687f9baaa2f827bf4136385d616c
hcnt <jacek.kusmierczyk1@gmail.com>
2021-01-19 03:56:06 +0100

    add ical, add first type

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,7 @@ from rest_framework.routers import DefaultRouter
 
 # Create a router and register our viewsets with it.
 import api.views as views
+from api.ical_views import EventFeed
 
 router = DefaultRouter()
 router.register('schedules', views.ScheduleViewSet)
@@ -12,6 +13,7 @@ router.register('commentReplies', views.CommentReplyViewSet)
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
+    path('schedules/<int:schedule_id>/to_webcal', EventFeed()),
     path('', include(router.urls)),
     path('auth/', include('rest_registration.api.urls')),
 ]
